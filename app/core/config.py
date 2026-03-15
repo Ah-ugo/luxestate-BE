e968913e8cf63fc6f5e1b61f2ea2cf20401f7c0d
mac <ahuekweprinceugo@gmail.com>
2026-03-15 09:40:03 +0100

    22nd commit

--- app/core/config.py
+++ app/core/config.py
@@ -9,8 +9,6 @@ class Settings(BaseSettings):
     MONGODB_URL: str
     DATABASE_NAME: str = "luxestate"
 
-
-
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 3000
