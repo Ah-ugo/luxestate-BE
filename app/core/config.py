9f490d451231593b547a6413ccb9306e27cb98c1
mac <ahuekweprinceugo@gmail.com>
2026-03-15 08:58:57 +0100

    21st commit

--- app/core/config.py
+++ app/core/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
 
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 3000
 
     CLOUDINARY_CLOUD_NAME: str
     CLOUDINARY_API_KEY: str
